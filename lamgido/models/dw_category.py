f2909de033a6d0dd8f9c4aa21fd48a9e2401cf77
Shicukoe <keotran15@gmail.com>
2026-03-04 16:47:05 +0700

    kết ngày 3

--- lamgido/models/dw_category.py
+++ lamgido/models/dw_category.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class DwCategory(models.Model):
@@ -10,3 +11,23 @@ class DwCategory(models.Model):
     description = fields.Text()
     active = fields.Boolean(default=True)
 
+    record_ids = fields.One2many(
+        'lamgido.dw.record',
+        'category_id',
+        string="Records"
+    )
+
+    record_count = fields.Integer(
+        compute="_compute_record_count"
+    )
+
+    def _compute_record_count(self):
+        for rec in self:
+            rec.record_count = len(rec.record_ids)
+
+    @api.constrains('name')
+    def _check_unique_name(self):
+        for rec in self:
+            domain = [('name', '=', rec.name), ('id', '!=', rec.id)]
+            if self.search_count(domain):
+                raise ValidationError("Category name must be unique.")
